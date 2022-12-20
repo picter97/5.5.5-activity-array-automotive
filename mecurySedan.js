7ac1d33b666e16a3d60d5697fde0b55062ad3732
picter <picter2esolv@gmail.com>
2022-12-20 16:39:45 -0500

    first commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,54 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicleBaseClass").Vehicle
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+//let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+//console.log(v.make)
+
+class Car extends VehicleModule {
+  constructor (make, model, year, color, mileage){
+    super(make, model, year, color, mileage);
+    this.maxPassengers = 5;
+    this.passengers = 0;
+    this.numberOfWheels = 4;
+    this.maxSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
+  start(){
+    if(this.fuel > 0){
+    return this.started = true
+  } else {
+    return this.started = false;
+  }
+
+  }
+  checkService(mileage){
+    if (this.mileage > 30000){
+      this.scheduleService = true
+      return this.scheduleService;
+    }
+  }
+    loadPassenger(num) {
+      if (this.passenger < this.maxPassengers) {
+          if ((num + this.passenger) <= this.maxPassengers) {
+              this.passenger = num;
+              return this.passenger;               
+          } else {
+              console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+          }
+      } else {
+          console.log(this.model + " " + this.make + " is full");
+      }
+    }
+
+  }
+
+
+
+
+
+
+
+
